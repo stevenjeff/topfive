756dd98ff979e884ea624287026401dbcd4a6b99
fangruizhang <stevenjeff@126.com>
2018-01-08 22:41:47 +0800

    init

---
+++ src/main/java/com/fangrui/topfive/TopfiveApplication.java
@@ -0,0 +1,12 @@
+package com.fangrui.topfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopfiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopfiveApplication.class, args);
+    }
+}
---
+++ src/test/java/com/fangrui/topfive/TopfiveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fangrui.topfive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TopfiveApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
