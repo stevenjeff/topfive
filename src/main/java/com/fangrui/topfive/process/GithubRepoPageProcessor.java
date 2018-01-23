71290d5d39c8a67984f063107019a66ea8ce4795
steven <stevenjefff@126.com>
2018-01-23 18:41:25 +0800

    spider init

--- src/main/java/com/fangrui/topfive/process/GithubRepoPageProcessor.java
+++ src/main/java/com/fangrui/topfive/process/GithubRepoPageProcessor.java
@@ -3,6 +3,7 @@ package com.fangrui.topfive.process;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.JsonFilePipeline;
 import us.codecraft.webmagic.processor.PageProcessor;
 
 /**
@@ -15,7 +16,19 @@ public class GithubRepoPageProcessor implements PageProcessor {
     private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
 
     public static void main(String[] args) {
-        Spider.create(new GithubRepoPageProcessor()).addUrl("https://github.com/code4craft").thread(5).run();
+        //Spider.create(new GithubRepoPageProcessor()).addUrl("https://github.com/code4craft").thread(5).run();
+
+        Spider.create(new GithubRepoPageProcessor())
+                //从https://github.com/code4craft开始抓
+                .addUrl("https://github.com/code4craft")
+                //设置Scheduler，使用Redis来管理URL队列
+                //.setScheduler(new RedisScheduler("localhost"))
+                //设置Pipeline，将结果以json方式保存到文件
+                .addPipeline(new JsonFilePipeline("D:\\data\\webmagic"))
+                //开启5个线程同时执行
+                .thread(5)
+                //启动爬虫
+                .run();
     }
 
     @Override
