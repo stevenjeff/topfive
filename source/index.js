f707d8b3ed35c4db823c22d45463a3867c3804e7
steven <stevenjefff@126.com>
2018-04-29 15:00:04 +0800

    spider init

--- source/index.js
+++ source/index.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
+import router from './router'
+import store from '@/vuex/store.js'   //vuex
+import api from '@/http/api.js'       //http请求
+import less from 'less'
 
 new Vue({
     el: '#app',
