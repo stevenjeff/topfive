0182b1a5c037064213a24dd7a84ac1ff65f6620c
steven <stevenjefff@126.com>
2018-04-29 14:44:59 +0800

    spider init

---
+++ source/common/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row">
+        <div class="col-xs-12">
+            <footer>
+                <p>All Rights Reserved/p>
+            </footer>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+
+</style>
