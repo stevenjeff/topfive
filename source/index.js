9fe4e896c0f574bb7000c460caba8e0a37de57d5
fangruizhang <stevenjeff@126.com>
2018-05-01 00:37:08 +0800

    init

--- source/index.js
+++ source/index.js
@@ -6,20 +6,24 @@ import {routes} from './router/routes'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import store from '@/vuex/store'
-
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-    routes,
-    store,
-    mode: 'history'
-});
 Vue.config.productionTip = false;
 //开启debug模式
 Vue.config.debug = true
 
+Vue.use(VueRouter);
+
+Vue.filter('currency', (value) => {
+    return '$' + value.toLocaleString();
+});
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+});
+
 new Vue({
     el: '#app',
     router,
+    store,
     render: h => h(App)
 })
