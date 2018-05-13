830c8adf04361850970243e943e90c0888beb5d0
steven <stevenjefff@126.com>
2018-05-13 14:07:44 +0800

    spider init

--- src/main/java/com/fangrui/controller/SpiderController.java
+++ src/main/java/com/fangrui/controller/SpiderController.java
@@ -4,6 +4,7 @@ import com.fangrui.cache.HutoolsTimedCache;
 import com.fangrui.service.SpiderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,25 +16,33 @@ public class SpiderController {
     @Autowired
     private SpiderService spiderService;
 
-    @GetMapping("/ali213")
-    public Object getAli213Data() throws Exception {
+    @GetMapping("/ali213/{interval}")
+    public Object getAli213Data(@PathVariable(name = "interval") Integer interval) throws Exception {
         if (HutoolsTimedCache.timedCache.get(HutoolsTimedCache.CACHE_ALI213_KEY) == null) {
             spiderService.get3DMData();
         }
-        return HutoolsTimedCache.timedCache.get(HutoolsTimedCache.CACHE_3DM_KEY);
+        return HutoolsTimedCache.timedCache.get(HutoolsTimedCache.CACHE_ALI213_DATE_INTERVAL_KEY + "_" + interval);
     }
 
-    @GetMapping("/3dm")
-    public Object get3DMData() throws Exception {
+    @GetMapping("/ali213DateRange")
+    public Object getAli213DataRage() throws Exception {
+        if (HutoolsTimedCache.timedCache.get(HutoolsTimedCache.CACHE_ALI213_DATE_INTERVALS) == null) {
+            spiderService.get3DMData();
+        }
+        return HutoolsTimedCache.timedCache.get(HutoolsTimedCache.CACHE_ALI213_DATE_INTERVALS);
+    }
+
+    @GetMapping("/3dm/{interval}")
+    public Object get3DMData(@PathVariable(name = "interval") Integer interval) throws Exception {
         if (HutoolsTimedCache.timedCache.get(HutoolsTimedCache.CACHE_3DM_KEY) == null) {
             spiderService.get3DMData();
         }
-        return HutoolsTimedCache.timedCache.get(HutoolsTimedCache.CACHE_3DM_KEY);
+        return HutoolsTimedCache.timedCache.get(HutoolsTimedCache.CACHE_3DM_DATE_INTERVAL_KEY + "_" + interval);
     }
 
     @GetMapping("/3dmDateRage")
     public Object get3DMDataRage() throws Exception {
-        if (HutoolsTimedCache.timedCache.get(HutoolsTimedCache.CACHE_3DM_KEY) == null) {
+        if (HutoolsTimedCache.timedCache.get(HutoolsTimedCache.CACHE_3DM_DATE_INTERVALS) == null) {
             spiderService.get3DMData();
         }
         return HutoolsTimedCache.timedCache.get(HutoolsTimedCache.CACHE_3DM_DATE_INTERVALS);
