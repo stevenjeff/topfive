10a4e8d42e689d3c83cbde6b0ecf26823fe850f2
fangruizhang <stevenjeff@126.com>
2018-05-22 00:07:27 +0800

    init

--- src/main/java/com/fangrui/repository/ResourceRepository.java
+++ src/main/java/com/fangrui/repository/ResourceRepository.java
@@ -5,15 +5,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author zhangfangrui
  * @description
  * @date 2018/5/21
  */
-@RepositoryRestResource(path = "resourcePage")
+@Repository
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
     /**
      * 分页查询
@@ -21,10 +20,7 @@ public interface ResourceRepository extends JpaRepository<Resource, Long> {
      * @param pageable
      * @return Page
      */
-    @Override
-    @RestResource(path = "resourcePage", rel = "resourcePage")
     public Page<Resource> findAll(Pageable pageable);
 
-    @RestResource(path = "findByName", rel = "findByName")
     public Page findByName(@Param(value = "name") String name, Pageable pageable);
 }
