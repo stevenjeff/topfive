dc177bce2d96c30831c7eb5b617df364f8731d78
steven <stevenjefff@126.com>
2018-05-21 11:43:41 +0800

    spider init

--- src/main/java/com/fangrui/TopfiveApplication.java
+++ src/main/java/com/fangrui/TopfiveApplication.java
@@ -2,13 +2,14 @@ package com.fangrui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
-public class TopfiveApplication {
+public class TopfiveApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TopfiveApplication.class, args);
