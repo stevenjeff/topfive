0342dda93ecb007431b21e2fcc901227392e8485
steven <stevenjefff@126.com>
2018-05-23 18:11:25 +0800

    spider init

--- source/views/share/SharePage.vue
+++ source/views/share/SharePage.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="container">
+        <app-Resource-Detail></app-Resource-Detail>
         <app-Resource-List></app-Resource-List>
     </div>
 </template>
 
 <script>
     import ResourceList from './ResourceList';
+    import ResourceDetail from './ResourceDetail';
 
     export default {
         name: "SharePage",
         components: {
-            appResourceList: ResourceList
+            appResourceList: ResourceList,
+            appResourceDetail: ResourceDetail
         }
     }
 </script>
